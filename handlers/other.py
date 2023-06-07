59e869a48c8991c455d8d99f06e7129f3748bf32
Artem Khrapuzov <tema102@mail.ru>
2023-06-07 12:17:11 +0600

    Added greeting and fixed bug

--- handlers/other.py
+++ handlers/other.py
@@ -8,7 +8,9 @@ async def echo_send(message: types.Message):
     if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')} \
             .intersection(set(json.load(open('маты.json')))) != set():
         await message.reply('Маты запрещены')
-    await message.delete()
+        await message.delete()
+    if message.text.lower() == 'привет':
+        await message.reply('И тебе привет!')
 
 
 def register_handlers_clients(dp: Dispatcher):
